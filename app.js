8a9a86fd5825257d904171e08ed51a1083f1b2d6
tastekim <tastekim@kakao.com>
2022-10-27 06:03:11 +0900

    세미콜론 삭제

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
-});
+})
 
 server.listen(3000, () => {
     console.log('listening on port 3000');
