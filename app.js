a7aaaa06a9b75a6d39d7960f68ac13bc90c5f8fe
tastekim <tastekim@kakao.com>
2022-10-29 23:09:31 +0900

    세미콜론 삭제

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server);
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
